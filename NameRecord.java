d2ab1bea9476008a3aa43b8e6ecf98c6d8c47ecf
Sterly Deracy <SDeracy@gmail.com>
2019-11-30 10:30:37 -0800

    Add files via upload

--- NameRecord.java
+++ NameRecord.java
@@ -1,55 +1,85 @@
+/***********************************************************************
+* Rachael Harvey and Sterly Deracy
+* CSC 220-01 
+* Project4- NameSurfer
+* 
+* Compilation: javac MyRandomWalkers.java
+* Execution: java MyRandomWalkers N
+*
+*
+*
+***********************************************************************/
+
 import java.util.Random;
 
 public class NameRecord {
 	
-	private String input;
-	private String name;
-	private int[] ranks;
-	private String[] values; //change this
-	private final int DECADE_INCREMENT = 10;
-	private final int START = 1900;
-	//decade = 0 and decade = 1 examples can be done using num % 100
+	private String name; // This is the name of the child name in the NameRecord object
+	private int[] ranks; // The list of ranks in popularity of the child name
+	private String[] rankStringValues; // Since the constructor will pass in a String line, this will be used to get the list of ranks of the child name and then parsed into int[] ranks.
+	private final int DECADE_INCREMENT = 10; // 
+	private final int START = 1900; // This is the actual beginning year of which the data for the child names were collected.
+	private final int DECADES = 11; // This is a constant that defines how many decades we expect to receive from String record, the String line passed into the constructor.
+	private final double LOWEST_RANK = 1100.0; // This is the lowest rank that a child name could have.
+	private final double PEN_RADIUS = .025; // This is an assigned value that tells the plot method how large it must draw its points. After testing, a value of .025 seemed to be an ideal point size.
 	
-	public NameRecord (String record) {
+	public NameRecord (String record) { // Constructor takes the data of the line passed in and assigns variable name with the name of the child and ranks with the ranks in popularity of the child name over 11 decades.
 		
-		name = record.substring(0, record.indexOf(" "));
-		record = record.substring(record.indexOf(" ")+1, record.length());
-		ranks = new int[11];
-		values = new String[11];	
+		name = record.substring(0, record.indexOf(" ")); // Name is assigned to the value between the beginning of the line and the first space, otherwise known as the name of the child.
+		record = record.substring(record.indexOf(" ")+1, record.length()); // Strings are immutable. Essentially, this takes record and excludes the name of the child from the line in order to parse the line for its ranks later on.
+		ranks = new int[DECADES]; // Instantiates int array ranks with a size of DECADES, which is how many ranks will be provided since every 10 years the rank is computed.
+		rankStringValues = new String[DECADES];	// Instantiates String array rankStringValues with a size of DECADES, which is how many ranks will be provided since every 10 years the rank is computed. 
 		
-		values = record.split(" ");
 		
-	  for (int i = 0; i < values.length; i++) {
+		/* 
+		* Because we know that the remaining part of String record will have 11 ranks,
+		* we are able to use the split function which will use the spaces in between the ranks as a delimeter and populate the array rankStringValues with the ranks of variable record.
+		*/
+		
+		rankStringValues = record.split(" "); 
+		
+		
+		/* 
+		* Using the array rankStringValues, we can then store the ranks into an int array.
+		* Since both arrays are the same size, we can use a for loop to parse the value of the string array and store the value(which is the rank) into the int array.
+		*/
+		
+	  for (int i = 0; i < rankStringValues.length; i++) {
 			
-			ranks[i] = Integer.parseInt(values[i]);
+			ranks[i] = Integer.parseInt(rankStringValues[i]);
 			
 		}
 	
 	}
 	
-	public String getName() {
+	public String getName() { // Returns the name of the child.
 		
 		return name;
 		
 	}
 	
-	public int getRank(int decade) {
+	public int getRank(int decade) { // Returns the rank of the child given the decade. It makes sure the value passed in is a valid value, otherwise a -1 is returned.
 		
-		if (decade >= 0 && decade <= 10) {
+		if (decade >= 0 && decade <= 10) { // If the decade passed in is in between 0 and 10, then it returns the rank in the decade specified.
 		
-		return ranks[decade];
+			return ranks[decade];
 		
 		}
 		
-		return -1;
+		return -1; // -1 is returned if the user enters an invalid decade.
 		
 	}
 	
-	public int bestYear() {
+	public int bestYear() { // Returns the year of which the child name had its highest popularity.
 		
-		int max = 0;
-		int maxIndex = 0;
-		int popularYear = 0;
+		int max = 0; // Stores the maximum rank found
+		int maxIndex = 0; // Stores the index of the maximum rank foound
+		int popularYear = 0; // Variable used to return the best year of the child name
+		
+		/* 
+		* This parses the array ranks to determine which index holds the maximum rank. 
+		*The index is important because it is used to calculate the variable popularYear.
+		*/
 		
 		for (int i = 0; i < ranks.length; i++) {
 			
@@ -62,17 +92,23 @@ public class NameRecord {
 	
 		}
 		
-		popularYear = START + (maxIndex*DECADE_INCREMENT);
-		return popularYear;
+		popularYear = START + (maxIndex*DECADE_INCREMENT); 
+		
+		/*
+		* Example: if the best year is 1940, the best rank will be held at index 4 because the decades are listed in numerical order. 
+		* Because a rank is recorded every 10 years, DECADE_INCREMENT is multiplied by index to get the amount of years(40), which is then added to 1900 to get the year 1940.
+		*/
+		
+		return popularYear; // Once popular year has been calculated, it's reported to the user.
 		
 	}
 	
-	public void plot() {
+	public void plot() { // Plots the popularity of the name's ranks using a line graph over 11 decades.
 		
-		Random randomColor = new Random();
-		int randomColorNum = randomColor.nextInt(13);
+		Random randomColor = new Random(); // randomColor is used to choose the color the graph is plotted in at random.
+		int randomColorNum = randomColor.nextInt(13); // Because there are 13 possible colors, randomColor will have 13 possible values, 0 to 12.
 		
-		switch (randomColorNum) {
+		switch (randomColorNum) { // Switch statement that chooses the color based on the value of randomColorNum.
 			
 			case 0:
 				StdDraw.setPenColor(StdDraw.BLACK);
@@ -108,41 +144,69 @@ public class NameRecord {
 				StdDraw.setPenColor(StdDraw.RED);
 				break;
 			case 11:
-				StdDraw.setPenColor(StdDraw.BOOK_LIGHT_BLUE); //should be white
+				StdDraw.setPenColor(StdDraw.BOOK_LIGHT_BLUE); // Substituted Book Light Blue for white.
 				break;
 			case 12:
 				StdDraw.setPenColor(StdDraw.YELLOW);
 				break;	
 			default:
-				StdDraw.setPenColor(StdDraw.BLACK);
-				System.out.println("AAAAA THERE IS A VERY BIG ISSUE HERE");
+				StdDraw.setPenColor(StdDraw.BLACK); // This should never be reached, but if it does, it will print an error statement, and set the default value to black.
+				System.out.println("Random color number invalid. Setting color to black.");
 				break;
 				
 		}
 		
-		System.out.println("color num: " + randomColorNum);
+		// Because the graph is plotted using a line graph, there are two sets of variables, the old point and the new point which is used to draw the line.
 		
-		for (int i = 0; i < ranks.length; i++) {
+		double oldX = 0; // Saves the data of the last point(or rank popularity) of coordinate X.
+		double oldY = 0; // Saves the data of the last point(or rank popularity) of coordinate Y.
+		double newX = 0; // Set to the current rank popularity of coordinate X. 
+		double newY = 0; // Stores the current rank popularity of coordinate Y.
+		
+		for (int i = 0; i < ranks.length; i++) { // This for loop plots all of the points on the graph. It also draws the line from point to point.
 			
-			double x = i/11.0;
-			double y = 1.0/ranks[i];
+			newX = i / (double)DECADES; // Because there are 11 decades/points being plotted on the graph, we will divide the graph into 11 areas to plot each point.
 			
-			if (ranks[i] == 0) {
+			/*
+			* Since we need to stay within the bounds of 0 and 1, dividing the child name's rank by 1100 creates a fraction that represents how far down/up it is in relation to other points. 
+			* This assumes that 1100 is the highest value, when in reality it is the lowest.
+			* So in order to counteract that, the value is subtracted from 1 which results in an accurate representation of the rank on the screen given its popularity that decade.
+			*/
 			
-			y = 0;
-			StdDraw.setPenRadius(.025);
-			StdDraw.point(0.5, 0.7);
-			//StdDraw.line(0,0,0.5,0.5);
+			newY = 1 - (ranks[i]/LOWEST_RANK); 
+								
+			if (ranks[i] == 0) { // the rank being 0 is a special case, in which it didn't even show up on the top 1000 most popular names. It is set to the very bottom of the graph by default.
+				
+				newY = 0;
+				StdDraw.setPenRadius(PEN_RADIUS);
+				StdDraw.point(newX, 0);
+				
+			} else {	// If the rank is not 0, we want to actually plot the rank in relation to its popularity so it uses the values newX and newY that we compute.		
 			
-			} else {			
-			StdDraw.setPenRadius(.025);
-			StdDraw.point(0.2,0.2);
-			//StdDraw.line(0,0,x,y);
-			
+					StdDraw.setPenRadius(PEN_RADIUS);
+					StdDraw.point(newX, newY);
+					
 			}
 			
-			System.out.println("X: " + x);
-			System.out.println("Y: " + y);
+			/* 
+			* Notice that oldX and oldY are assigned 0 by default. 
+			* Because we do not want to have an issue where a line is created when there is only one point, 
+			* we check to make sure that at least one of the values oldX or oldY not equal to 0, which would indicate that we now have plotted more than one point.
+			* There is the possibility of not drawing a line if the first point has a ranking of 0, since the x would be 0 and the y would be 0. This is where the second check comes in.
+			* We only want to draw the line if there is more than point on the screen, so we check if i-1 is equal to 0, which is really just
+			* asking if we have plotted more than one point. This assures that even if the first point has a ranking of 0,
+			* we will still be able to draw a line from one point to the next.
+			*/
+			
+			if ((oldX != 0 || oldY != 0) || ((i - 1) == 0)) { 
+				
+				StdDraw.line(oldX, oldY, newX, newY);
+				
+			}
+			
+			oldX = newX; // Before the loop re-enters, we set oldX to newX so that the "current value" x coordinate then becomes the old value with the next iteration.
+			oldY = newY; // Before the loop re-enters, we set oldX to newX so that the "current value" x coordinate then becomes the old value with the next iteration.
+			
 			
 		}
 		
--- NameSurfer.java
+++ NameSurfer.java
@@ -1,97 +1,180 @@
+/***********************************************************************
+* Rachael Harvey and Sterly Deracy
+* CSC 220-01 
+* Project4- NameSurfer
+* 
+* Compilation: javac NameSurfer.java
+* Execution: java NameSurfer
+*
+* Simulates how far away after N steps random walk
+*
+*
+***********************************************************************/
+
 import java.util.Scanner;
 import java.io.File;
 
 public class NameSurfer {
-	 
+	
+	// Helper method that checks to see if the name entered by the user is within the name_data.txt file and returns the index of the name or -1 if the name is not found.
+  private static int checkNames(NameRecord[] names, String nameChoice) {
+		
+		// Loops through the name_data.txt file.
+		for (int i = 0; i < names.length-1; i++) {
+					
+		  // If the name entered by the user and the name at index i are the same, regardless the case, then the index of the name is returned. 
+			if ((names[i].getName()).equalsIgnoreCase(nameChoice)) {
+				
+				return i;  // Returns the index of the name 
+				
+			}
+					
+		}
+		
+		return -1;  // Returns -1 if the name is not found in the file.
+		
+	}
+	
+	// Main method that runs the program continuously until the user enters 5.
 	public static void main(String[] args) throws Exception {
 		
-		File file = new File("name_data.txt");
-		Scanner fileScanner = new Scanner(file);
-		Scanner input = new Scanner(System.in);
-		int userSelection = 0;
-		String nameChoice = "";
-		NameRecord[] names = new NameRecord[4430];
-		int count = 0;
+		File file = new File("name_data.txt");    // Creates an instance of the name_data.txt file text.
+		Scanner fileScanner = new Scanner(file);  // Scanner object created to parse the contents of the file.
+		Scanner input = new Scanner(System.in);   // Creates a scanner object that will be used to read input from the user. 
 		
+		int userSelection = 0;                               // Initializes the user's menu selection to 0.
+		String nameChoice = "";                              // String nameChoice that will hold the user's input for the name.
+    final int LINES_IN_FILE = 4430;                      // final int that contains the number of lines in the name_data.txt file.
+		NameRecord[] names = new NameRecord[LINES_IN_FILE];  // Array of NameRecord objects set to the number of lines in the file.
+		int count = 0;                                       // Count variable is used to increment the array index to populate the elements of the array with NameRecord objects.
+		int namePlace = 0;                                   // Index of the name chosen by the user in the names array.
 		
+		// While there is still another line in the file. 
 		while (fileScanner.hasNextLine()) {
 		
-		NameRecord childName = new NameRecord(fileScanner.nextLine());
-		if (!childName.equals(" ")) {
-		
-		names[count] = childName;
-		count++;
-		
-		}
-		
-		}
-		
-		while (userSelection != 5) {
-		
-		System.out.println("1 - Find best year for a name");
-		System.out.println("2 - Find best year for a name");
-		System.out.println("3 - Find best year for a name");
-		System.out.println("4 - Find best year for a name");
-		System.out.println("5 - Quit");
-		System.out.println("Enter your selection.");
-		userSelection = input.nextInt();
-		input.nextLine();
-		
-		switch (userSelection) {
+		  // Create an object of the line and add it to the array. 
+			NameRecord childName = new NameRecord(fileScanner.nextLine());
 			
-			case 1: 
-				System.out.println("Enter a name: ");
-				nameChoice = input.nextLine();
-				System.out.println(nameChoice);
-				for (int i = 0; i < names.length-1; i++) {
-					
-					if ((names[i].getName()).equals(nameChoice)) {
-							
-							System.out.println(names[i].bestYear());
-							
-					}
-						
-				}
-				break;
-			case 2:	//add boolean flag
-				System.out.println("Enter a name: ");
-				nameChoice = input.nextLine();
-				System.out.println(nameChoice);
-				int bestDecade = 0;
-				for (int i = 0; i < names.length-1; i++) {
-					
-					if ((names[i].getName()).equals(nameChoice)) {
-							
-							bestDecade = names[i].bestYear()%100/10;
-							System.out.println(names[i].getRank(bestDecade));
-							
-					}
-						
-				}
-				break;
-			case 3:
-				System.out.println("Enter a name: ");
-				nameChoice = input.nextLine();
-				System.out.println(nameChoice);
-				for (int i = 0; i < names.length-1; i++) {
-					
-					if ((names[i].getName()).equalsIgnoreCase(nameChoice)) {
-							
-							names[i].plot();
-							
-					}
-						
-				}
-				break;
-			case 4:
-				StdDraw.clear();
-				break;
-			case 5:
-				break;
+			// If the line in the file is not empty the element of the array at index count is set to the NameRecord object childName and increments count by 1.
+			if (!childName.equals(" ")) {
+		
+				names[count] = childName;  // The element of the array at index count is set to the NameRecord object childName
+				count++;                   // Count is incremented by 1.
+		
+			}
+		
 		}
+		
+		// While the user does not enter 5(quit) the menu continues to be prompted to the user. 
+		while (userSelection != 5) {
+			
+			
+			// Menu options for the user. 
+			System.out.println("1 - Find best year for a name");
+			System.out.println("2 - Find best rank for a name");
+			System.out.println("3 - Plot popularity of a name");
+			System.out.println("4 - Clear plot");
+			System.out.println("5 - Quit");
+			System.out.print("Enter your selection. ");
+			userSelection = input.nextInt();  // Reads in the user's selection and sets it to userSelection.
+			System.out.println("");           // By adding a new line after the input the user has a more readible application. 
+			input.nextLine();                 // Prevents the scanner from skipping over necessary input.
+			
+			// If the user's selection option is between 1 and 3 inclusive.
+			if (userSelection >= 1 && userSelection <= 3) {
+				
+				System.out.print("Enter a name: ");         // Prompts the user to enter a name.
+				nameChoice = input.nextLine();              // nameChoice is assigned to the user input. 
+				
+				/* Variable namePlace is assigned to the index of the chosen word in the object array if it is found, otherwise it is assigned -1. The checkNames method is invoked using
+				*  the object array and the user's name choice and determines if the name chosen is within the file.
+				*/
+				namePlace = checkNames(names, nameChoice);  
+				
+			}
+			
+			// Switch statements that will be used to determine the program function based on the user input.
+			switch (userSelection) { 
+				
+				// If the user enters 1 the program outputs the best year for the user-specified name.
+				case 1: 
+				    
+						// If the user-specified name is found in the file outputs the best year for the user-specified name.
+						if (namePlace != -1) {
+					
+				      // Displays the best year for the user-specified name.
+							System.out.println("The best year for " + nameChoice + " is: " + names[namePlace].bestYear() + "\n");
+								
+						// If the user-specified name is not found in the file an error message is printed to the user.
+						}  else {
+							
+								System.out.println("Could not find name.\n");
+							
+						}
+							
+					break; // End of case 1
+				
+        // If the user enters 2 the program outputs the best rank for a user-specified name or an error message, if the name is not found in the file.				
+				case 2:	
+				
+					int bestDecade = 0;  // Int variable that will be used to calculate the best rank for the name. This will be needed later since the getRank() method needs to have a decade argument
+
+            // -1 is the default value if a name is not found in the file. If namePlace is not -1 then that means the name is found in the file.
+						if (namePlace != -1) {
+								
+							bestDecade = (names[namePlace].bestYear()%100) / 10;  // Calculates the best decade for a name by taking the remainder of the best year of the name modulo 100 then divides that number by 10.
+							// Once we have the best decade, we pass the decade into the method getRank which then outputs the best rank of the name. 
+							System.out.println("The best rank for " + nameChoice + " is: " + names[namePlace].getRank(bestDecade) + "\n");
+								
+						} else {
+							
+								System.out.println("Could not find name.\n");
+							
+						}
+							
+					break;  // End of case 2
+					
+				// If the user enters 3 the program plots the popularity of the name of 11 decades. If the name has not been found an error message is printed the program does not plot anything. 	
+				case 3:
+				
+				    // If the name has been found in the file the program plots the popularity of the name over 11 decades.
+						if (namePlace != -1) {		
+								
+							names[namePlace].plot();  // Plots popularity 
+						
+						}  else {
+							
+								System.out.println("Could not find name.\n");  // Prints an error message telling the user the name was not found.
+							
+						}
+							
+					break;  // End of case 3
+				
+				// If the user enters 4 the program clears the plot.
+				case 4:
+				
+					StdDraw.clear();  // Clears the plot
+					System.out.println("Plot cleared.\n");
+					
+					break;  // End of case 4
+				
+        // If the user enters 5 then the Scanner object invokes the close method.				
+				case 5:
+				
+					input.close();
+					
+					break;  // End of case 5
+				
+        // If the user enters a selection other than 1, 2, 3, 4 or 5.				
+				default:
+				
+					System.out.println("Invalid Entry.\n");  // Displays an error message telling the user that their input is invalid.
+					
+					break;  // End of default case 
+			}
 		
 		}
 		
 	}
-	
+
 }
